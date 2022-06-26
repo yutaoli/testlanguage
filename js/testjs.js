26db12409d7b58df8c9b0845ebb49e6416396f7c
yutaoli <1213617849@qq.com>
2022-06-26 10:10:52 +0800

    6

--- js/testjs.js
+++ js/testjs.js
@@ -1,3 +1,4 @@
+// https://www.programiz.com/javascript/examples
 function problem3() {
     const number1 = 2.25;
     const number2 = -1;
@@ -24,15 +25,72 @@ function problem4() {
     // 
     if (disriminant > 0) {
         root1 = (-b + Math.sqrt(disriminant)) / (2 * a);
-        root2 = (-b + Math.sqrt(disriminant)) / (2 * a);
+        root2 = (-b - Math.sqrt(disriminant)) / (2 * a);
 
         // result
         console.log(`The roots of quadratic equation are ${root1} and ${root2}`)
     }
     else if (disriminant == 0) {
         root1 = root2 = -b / (2 * a);
+
+        // result
+        console.log(`The roots of quadratic queation are ${root1} and ${root2}`)
+    }
+    else {
+        let realpart = (-b / (2 * a)).toFixed(2);
+        let imagPart = (Math.sqrt(-disriminant) / (2 * a)).toFixed(2);
+
+        //result
+        console.log(`The roots of quadratic equation are ${realpart}+${imagPart}i and ${realpart}-${imagPart}i`)
     }
 
 }
 
-problem3()
+function problem5() {
+    const number = parseInt(prompt("Enter a number: "));
+
+    // check 
+    if (number > 0) {
+        console.log("The number is Positive");
+    }
+    else if (number == 0) {
+        console.log("The number is 0");
+    }
+    else {
+        console.log("The number is negative");
+    }
+}
+
+function problem6(){
+    const a = 5;
+    console.log(a);
+
+    // constants are block-scoped
+    {
+        const a = 50;
+        console.log(a);
+    }
+    console.log(a);
+
+    const arr = ['zero', 'one', 'two'];
+    console.log(arr);
+
+    // add elements to arr array
+    arr[3] = 'three';
+    console.log(`arr length:${arr.length}, arr:${arr}`);
+
+    arr.push('four');// add
+    console.log(`arr length:${arr.length}, arr:${arr}`);
+
+    arr.splice(0,2);// delete
+    console.log(`arr length:${arr.length}, arr:${arr}`);
+
+    arr[10] = 'ten';// modify/add
+    console.log(`arr length:${arr.length}, arr:${arr}`);
+
+    var lookupstring = 'one';// find 
+    var i = arr.indexOf(lookupstring);
+    console.log(`${lookupstring} of in arr is ${i}`)
+}
+
+problem6()

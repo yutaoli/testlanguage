5d4e9d3e0b9b9e1f8cee3233115eeee7c2aebf9e
yutaoli <1213617849@qq.com>
2022-07-01 16:03:40 +0800

    c

--- js/DOM-Scripting/README.md
+++ js/DOM-Scripting/README.md
@@ -1,2 +1,5 @@
 # DOM-Scripting
 This repository accompanies 《JavaScript DOM编程艺术》 by Jeremy Keith and Jeffrey Sambells (Apress, 2010).</br></br>每个章节都有对应的练习例子
+git clone
+浏览器打开html
+F12->console调试
--- js/DOM-Scripting/chapter12/domsters/scripts/global.js
+++ js/DOM-Scripting/chapter12/domsters/scripts/global.js
@@ -5,59 +5,60 @@ function addLoadEvent(func) {
   if (typeof window.onload != 'function') {
     window.onload = func;
   } else {
-    window.onload = function() {
+    window.onload = function () {
       oldonload();
       func();
     }
   }
 }
 
-function insertAfter(newElement,targetElement) {
+function insertAfter(newElement, targetElement) {
   var parent = targetElement.parentNode;
   if (parent.lastChild == targetElement) {
     parent.appendChild(newElement);
   } else {
-    parent.insertBefore(newElement,targetElement.nextSibling);
+    parent.insertBefore(newElement, targetElement.nextSibling);
   }
 }
 
-function addClass(element,value) {
+function addClass(element, value) {
   if (!element.className) {
     element.className = value;
   } else {
     newClassName = element.className;
-    newClassName+= " ";
-    newClassName+= value;
+    newClassName += " ";
+    newClassName += value;
     element.className = newClassName;
   }
 }
 
 function highlightPage() {
   if (!document.getElementsByTagName) return false;
-  if (!document.getElementById) return false;  
+  if (!document.getElementById) return false;
   var headers = document.getElementsByTagName('header');
   if (headers.length == 0) return false;
   var navs = headers[0].getElementsByTagName('nav');
   if (navs.length == 0) return false;
-  
+
   var links = navs[0].getElementsByTagName("a");
-  for (var i=0; i<links.length; i++) {
-	  var linkurl;
-	  for (var i=0; i<links.length; i++) {
-	    linkurl = links[i].getAttribute("href");
-	    if (window.location.href.indexOf(linkurl) != -1) {
-	      links[i].className = "here";
-	      var linktext = links[i].lastChild.nodeValue.toLowerCase();
-	      document.body.setAttribute("id",linktext);
-	    }
-	  }
+  for (var i = 0; i < links.length; i++) {
+    var linkurl;
+    for (var i = 0; i < links.length; i++) {
+      linkurl = links[i].getAttribute("href");
+      if (window.location.href.indexOf(linkurl) != -1) {
+        links[i].className = "here";
+        var linktext = links[i].lastChild.nodeValue.toLowerCase();
+        document.body.setAttribute("id", linktext);
+      }
+    }
   }
+  //console.log(document)
 }
 
 
 // Home
 
-function moveElement(elementID,final_x,final_y,interval) {
+function moveElement(elementID, final_x, final_y, interval) {
   if (!document.getElementById) return false;
   if (!document.getElementById(elementID)) return false;
   var elem = document.getElementById(elementID);
@@ -76,25 +77,25 @@ function moveElement(elementID,final_x,final_y,interval) {
     return true;
   }
   if (xpos < final_x) {
-    var dist = Math.ceil((final_x - xpos)/10);
+    var dist = Math.ceil((final_x - xpos) / 10);
     xpos = xpos + dist;
   }
   if (xpos > final_x) {
-    var dist = Math.ceil((xpos - final_x)/10);
+    var dist = Math.ceil((xpos - final_x) / 10);
     xpos = xpos - dist;
   }
   if (ypos < final_y) {
-    var dist = Math.ceil((final_y - ypos)/10);
+    var dist = Math.ceil((final_y - ypos) / 10);
     ypos = ypos + dist;
   }
   if (ypos > final_y) {
-    var dist = Math.ceil((ypos - final_y)/10);
+    var dist = Math.ceil((ypos - final_y) / 10);
     ypos = ypos - dist;
   }
   elem.style.left = xpos + "px";
   elem.style.top = ypos + "px";
-  var repeat = "moveElement('"+elementID+"',"+final_x+","+final_y+","+interval+")";
-  elem.movement = setTimeout(repeat,interval);
+  var repeat = "moveElement('" + elementID + "'," + final_x + "," + final_y + "," + interval + ")";
+  elem.movement = setTimeout(repeat, interval);
 }
 
 function prepareSlideshow() {
@@ -103,39 +104,40 @@ function prepareSlideshow() {
   if (!document.getElementById("intro")) return false;
   var intro = document.getElementById("intro");
   var slideshow = document.createElement("div");
-  slideshow.setAttribute("id","slideshow");
+  slideshow.setAttribute("id", "slideshow");
   var frame = document.createElement("img");
-  frame.setAttribute("src","images/frame.gif");
-  frame.setAttribute("alt","");
-  frame.setAttribute("id","frame");
+  frame.setAttribute("src", "images/frame.gif");
+  frame.setAttribute("alt", "");
+  frame.setAttribute("id", "frame");
   slideshow.appendChild(frame);
   var preview = document.createElement("img");
-  preview.setAttribute("src","images/slideshow.gif");
-  preview.setAttribute("alt","a glimpse of what awaits you");
-  preview.setAttribute("id","preview");
+  preview.setAttribute("src", "images/slideshow.gif");
+  preview.setAttribute("alt", "a glimpse of what awaits you");
+  preview.setAttribute("id", "preview");
   slideshow.appendChild(preview);
-  insertAfter(slideshow,intro);
+  insertAfter(slideshow, intro);
   var links = document.getElementsByTagName("a");
-  for (var i=0; i<links.length; i++) {
-    links[i].onmouseover = function() {
+  for (var i = 0; i < links.length; i++) {
+    links[i].onmouseover = function () {
       var destination = this.getAttribute("href");
       if (destination.indexOf("index.html") != -1) {
-        moveElement("preview",0,0,5);
+        moveElement("preview", 0, 0, 5);
       }
       if (destination.indexOf("about.html") != -1) {
-        moveElement("preview",-150,0,5);
+        moveElement("preview", -150, 0, 5);
       }
       if (destination.indexOf("photos.html") != -1) {
-        moveElement("preview",-300,0,5);
+        moveElement("preview", -300, 0, 5);
       }
       if (destination.indexOf("live.html") != -1) {
-        moveElement("preview",-450,0,5);
+        moveElement("preview", -450, 0, 5);
       }
       if (destination.indexOf("contact.html") != -1) {
-        moveElement("preview",-600,0,5);
+        moveElement("preview", -600, 0, 5);
       }
     }
   }
+  //console.log(document)
 }
 
 
@@ -143,7 +145,7 @@ function prepareSlideshow() {
 
 function showSection(id) {
   var sections = document.getElementsByTagName("section");
-  for (var i=0; i<sections.length; i++ ) {
+  for (var i = 0; i < sections.length; i++) {
     if (sections[i].getAttribute("id") != id) {
       sections[i].style.display = "none";
     } else {
@@ -161,12 +163,12 @@ function prepareInternalnav() {
   if (navs.length == 0) return false;
   var nav = navs[0];
   var links = nav.getElementsByTagName("a");
-  for (var i=0; i<links.length; i++ ) {
+  for (var i = 0; i < links.length; i++) {
     var sectionId = links[i].getAttribute("href").split("#")[1];
     if (!document.getElementById(sectionId)) continue;
     document.getElementById(sectionId).style.display = "none";
     links[i].destination = sectionId;
-    links[i].onclick = function() {
+    links[i].onclick = function () {
       showSection(this.destination);
       return false;
     }
@@ -180,7 +182,7 @@ function showPic(whichpic) {
   if (!document.getElementById("placeholder")) return true;
   var source = whichpic.getAttribute("href");
   var placeholder = document.getElementById("placeholder");
-  placeholder.setAttribute("src",source);
+  placeholder.setAttribute("src", source);
   if (!document.getElementById("description")) return false;
   if (whichpic.getAttribute("title")) {
     var text = whichpic.getAttribute("title");
@@ -200,16 +202,16 @@ function preparePlaceholder() {
   if (!document.getElementById) return false;
   if (!document.getElementById("imagegallery")) return false;
   var placeholder = document.createElement("img");
-  placeholder.setAttribute("id","placeholder");
-  placeholder.setAttribute("src","images/placeholder.gif");
-  placeholder.setAttribute("alt","my image gallery");
+  placeholder.setAttribute("id", "placeholder");
+  placeholder.setAttribute("src", "images/placeholder.gif");
+  placeholder.setAttribute("alt", "my image gallery");
   var description = document.createElement("p");
-  description.setAttribute("id","description");
+  description.setAttribute("id", "description");
   var desctext = document.createTextNode("Choose an image");
   description.appendChild(desctext);
   var gallery = document.getElementById("imagegallery");
-  insertAfter(description,gallery);
-  insertAfter(placeholder,description);
+  insertAfter(description, gallery);
+  insertAfter(placeholder, description);
 }
 
 function prepareGallery() {
@@ -218,8 +220,8 @@ function prepareGallery() {
   if (!document.getElementById("imagegallery")) return false;
   var gallery = document.getElementById("imagegallery");
   var links = gallery.getElementsByTagName("a");
-  for ( var i=0; i < links.length; i++) {
-    links[i].onclick = function() {
+  for (var i = 0; i < links.length; i++) {
+    links[i].onclick = function () {
       return showPic(this);
     }
   }
@@ -231,12 +233,12 @@ function prepareGallery() {
 function stripeTables() {
   if (!document.getElementsByTagName) return false;
   var tables = document.getElementsByTagName("table");
-  for (var i=0; i<tables.length; i++) {
+  for (var i = 0; i < tables.length; i++) {
     var odd = false;
     var rows = tables[i].getElementsByTagName("tr");
-    for (var j=0; j<rows.length; j++) {
+    for (var j = 0; j < rows.length; j++) {
       if (odd == true) {
-        addClass(rows[j],"odd");
+        addClass(rows[j], "odd");
         odd = false;
       } else {
         odd = true;
@@ -246,14 +248,14 @@ function stripeTables() {
 }
 
 function highlightRows() {
-  if(!document.getElementsByTagName) return false;
+  if (!document.getElementsByTagName) return false;
   var rows = document.getElementsByTagName("tr");
-  for (var i=0; i<rows.length; i++) {
+  for (var i = 0; i < rows.length; i++) {
     rows[i].oldClassName = rows[i].className
-    rows[i].onmouseover = function() {
-      addClass(this,"highlight");
+    rows[i].onmouseover = function () {
+      addClass(this, "highlight");
     }
-    rows[i].onmouseout = function() {
+    rows[i].onmouseout = function () {
       this.className = this.oldClassName
     }
   }
@@ -264,7 +266,7 @@ function displayAbbreviations() {
   var abbreviations = document.getElementsByTagName("abbr");
   if (abbreviations.length < 1) return false;
   var defs = new Array();
-  for (var i=0; i<abbreviations.length; i++) {
+  for (var i = 0; i < abbreviations.length; i++) {
     var current_abbr = abbreviations[i];
     if (current_abbr.childNodes.length < 1) continue;
     var definition = current_abbr.getAttribute("title");
@@ -299,9 +301,9 @@ function displayAbbreviations() {
 function focusLabels() {
   if (!document.getElementsByTagName) return false;
   var labels = document.getElementsByTagName("label");
-  for (var i=0; i<labels.length; i++) {
+  for (var i = 0; i < labels.length; i++) {
     if (!labels[i].getAttribute("for")) continue;
-    labels[i].onclick = function() {
+    labels[i].onclick = function () {
       var id = this.getAttribute("for");
       if (!document.getElementById(id)) return false;
       var element = document.getElementById(id);
@@ -312,36 +314,40 @@ function focusLabels() {
 
 function resetFields(whichform) {
   if (Modernizr.input.placeholder) return;
-  for (var i=0; i<whichform.elements.length; i++) {
+
+  for (var i = 0; i < whichform.elements.length; i++) {
     var element = whichform.elements[i];
+
     if (element.type == "submit") continue;
     if (!element.getAttribute('placeholder')) continue;
-    element.onfocus = function() {
-    if (this.value == this.getAttribute('placeholder')) {
-      this.value = "";
-     }
+    element.onfocus = function () {
+      if (this.value == this.getAttribute('placeholder')) {
+        this.value = "";
+      }
     }
-    element.onblur = function() {
+    element.onblur = function () {
       if (this.value == "") {
         this.value = this.getAttribute('placeholder');
       }
+      //this.value=''
+      //console.log(this)
     }
     element.onblur();
   }
 }
 
 function validateForm(whichform) {
-  for (var i=0; i<whichform.elements.length; i++) {
+  for (var i = 0; i < whichform.elements.length; i++) {
     var element = whichform.elements[i];
     if (element.getAttribute("required") == 'required') {
       if (!isFilled(element)) {
-        alert("Please fill in the "+element.name+" field.");
+        alert("Please fill in the " + element.name + " field.");
         return false;
       }
     }
     if (element.getAttribute("type") == 'email') {
       if (!isEmail(element)) {
-        alert("The "+element.name+" field must be a valid email address.");
+        alert("The " + element.name + " field must be a valid email address.");
         return false;
       }
     }
@@ -358,10 +364,10 @@ function isEmail(field) {
 }
 
 function prepareForms() {
-  for (var i=0; i<document.forms.length; i++) {
+  for (var i = 0; i < document.forms.length; i++) {
     var thisform = document.forms[i];
     resetFields(thisform);
-    thisform.onsubmit = function() {
+    thisform.onsubmit = function () {
       if (!validateForm(this)) return false;
       var article = document.getElementsByTagName('article')[0];
       if (submitFormWithAjax(this, article)) return false;
@@ -377,31 +383,31 @@ function getHTTPObject() {
   if (typeof XMLHttpRequest == "undefined")
     XMLHttpRequest = function () {
       try { return new ActiveXObject("Msxml2.XMLHTTP.6.0"); }
-        catch (e) {}
+      catch (e) { }
       try { return new ActiveXObject("Msxml2.XMLHTTP.3.0"); }
-        catch (e) {}
+      catch (e) { }
       try { return new ActiveXObject("Msxml2.XMLHTTP"); }
-        catch (e) {}
+      catch (e) { }
       return false;
-  }
+    }
   return new XMLHttpRequest();
 }
 
 function displayAjaxLoading(element) {
-    // Remove the existing content.
+  // Remove the existing content.
   while (element.hasChildNodes()) {
-      element.removeChild(element.lastChild);
+    element.removeChild(element.lastChild);
   }
   //  Create a loading image.
   var content = document.createElement("img");
-  content.setAttribute("src","images/loading.gif");
-  content.setAttribute("alt","Loading...");
+  content.setAttribute("src", "images/loading.gif");
+  content.setAttribute("alt", "Loading...");
   // Append the loading element.
   element.appendChild(content);
 }
 
-function submitFormWithAjax( whichform, thetarget ) {
-  
+function submitFormWithAjax(whichform, thetarget) {
+
   var request = getHTTPObject();
   if (!request) { return false; }
 
@@ -411,7 +417,7 @@ function submitFormWithAjax( whichform, thetarget ) {
   // Collect the data.
   var dataParts = [];
   var element;
-  for (var i=0; i<whichform.elements.length; i++) {
+  for (var i = 0; i < whichform.elements.length; i++) {
     element = whichform.elements[i];
     dataParts[i] = element.name + '=' + encodeURIComponent(element.value);
   }
@@ -422,21 +428,21 @@ function submitFormWithAjax( whichform, thetarget ) {
 
   request.onreadystatechange = function () {
     if (request.readyState == 4) {
-        if (request.status == 200 || request.status == 0) {
-          var matches = request.responseText.match(/<article>([\s\S]+)<\/article>/);
-          if (matches.length > 0) {
-            thetarget.innerHTML = matches[1];
-          } else {
-            thetarget.innerHTML = '<p>Oops, there was an error. Sorry.</p>';
-          }
+      if (request.status == 200 || request.status == 0) {
+        var matches = request.responseText.match(/<article>([\s\S]+)<\/article>/);
+        if (matches.length > 0) {
+          thetarget.innerHTML = matches[1];
         } else {
-          thetarget.innerHTML = '<p>' + request.statusText + '</p>';
+          thetarget.innerHTML = '<p>Oops, there was an error. Sorry.</p>';
         }
+      } else {
+        thetarget.innerHTML = '<p>' + request.statusText + '</p>';
+      }
     }
   };
 
   request.send(data);
-   
+
   return true;
 };
 

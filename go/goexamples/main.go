0336733bdfdb8b0f4289e2ed0170f6f6000bc394
yutaoli <1213617849@qq.com>
2022-07-16 16:46:35 +0800

    c

--- go/goexamples/main.go
+++ go/goexamples/main.go
@@ -4,9 +4,24 @@
 package main
 
 import (
+	"bufio"
+	"crypto/sha256"
+	b64 "encoding/base64"
+	"encoding/json"
 	"errors"
 	"fmt"
+	"io"
 	"math"
+	"math/rand"
+	"net"
+	"net/url"
+	"os"
+	"path/filepath"
+	"sort"
+	"strconv"
+	"strings"
+	"sync"
+	"sync/atomic"
 	"time"
 )
 
@@ -804,6 +819,850 @@ func Tickers() {
 	fmt.Println("Ticker stopped")
 }
 
+func Workersworker(id int, jobs <-chan int, result chan<- string) {
+	fmt.Println("worker", id, "begin")
+	var out string
+
+	for j := range jobs {
+		fmt.Println("worker", id, "started job", j)
+		time.Sleep(time.Second)
+		fmt.Println("worker", id, "finished job", j)
+
+		out = fmt.Sprintf("worker%d,j:%d", id, j)
+		result <- out
+	}
+	fmt.Println("worker", id, "out")
+}
+
+func Workers() {
+	const numJobs = 5
+	jobs := make(chan int, numJobs)
+	results := make(chan string, numJobs)
+
+	for w := 1; w <= 3; w++ {
+		go Workersworker(w, jobs, results) // 3个worker去抢写入jobs的数据，怎么实现的？加锁？TODO
+	}
+
+	for j := 1; j <= numJobs; j++ {
+		jobs <- j
+	}
+	close(jobs)
+
+	for a := 1; a <= numJobs; a++ {
+		out := <-results
+		fmt.Println(out)
+	}
+}
+
+func waitgroupWorker(id int) {
+	fmt.Printf("worker %d starting\n", id)
+	time.Sleep(time.Second)
+	fmt.Printf("worker %d done\n", id)
+}
+
+func WaitGroup() {
+	var wg sync.WaitGroup // 类似信号量
+
+	wg.Add(5)
+	for i := 1; i <= 5; i++ {
+		//wg.Add(1) //计数器的值，初始化为0，Add(n)就是+n
+
+		fmt.Println("i=", i)
+		/* //解法1：
+		j := i // 因为下面的i用的是for里的instance，所以如果后面i变化了，但go routine才启动，就会读的变化后的i；这种放到里面的i:=i就是新建一个局部变量i，和for的i是两个变量
+		go func() {
+			defer wg.Done() // 就是-1
+			waitgroupWorker(j)
+		}() */
+
+		// 解法2
+		go func(i int) {
+			defer wg.Done() // 就是-1
+			waitgroupWorker(i)
+		}(i)
+	}
+
+	wg.Wait() // 就是不为零则轮询的死循环：while(!=0){}
+}
+
+func RateLimiting() {
+	requests := make(chan int, 5)
+	for i := 1; i <= 5; i++ {
+		requests <- i
+	}
+	close(requests)
+
+	limiter := time.Tick(200 * time.Millisecond)
+
+	for req := range requests {
+		<-limiter
+		fmt.Println("request", req, time.Now())
+	}
+
+	burstyLimiter := make(chan time.Time, 3)
+	for i := 0; i < 3; i++ {
+		burstyLimiter <- time.Now()
+	}
+	go func() {
+		for t := range time.Tick(200 * time.Microsecond) {
+			burstyLimiter <- t
+		}
+	}()
+
+	burstyRequests := make(chan int, 5)
+	for i := 1; i <= 5; i++ {
+		burstyRequests <- i
+	}
+	close(burstyRequests)
+	for req := range burstyRequests {
+		<-burstyLimiter
+		fmt.Println("request", req, time.Now())
+	}
+
+}
+
+func Atomic() {
+	var ops uint64
+	var wg sync.WaitGroup
+
+	for i := 0; i < 50; i++ {
+		wg.Add(1)
+
+		go func() {
+			for c := 0; c < 1000; c++ {
+				atomic.AddUint64(&ops, 1)
+			}
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+	fmt.Println("ops:", ops)
+}
+
+type Container struct {
+	mu       sync.Mutex
+	counters map[string]int
+}
+
+func (c *Container) inc(name string) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.counters[name]++
+}
+
+func Mutex() {
+	c := Container{
+		counters: map[string]int{"a": 0, "b": 0},
+	}
+	var wg sync.WaitGroup
+	doIncrement := func(name string, n int) {
+		for i := 0; i < n; i++ {
+			c.inc(name)
+		}
+		wg.Done()
+	}
+
+	wg.Add(3)
+	go doIncrement("a", 10000)
+	go doIncrement("a", 10000)
+	go doIncrement("b", 10000)
+
+	wg.Wait()
+	fmt.Println(c.counters)
+}
+
+type StatefulGoroutineReadOp struct {
+	key  int
+	resp chan int
+}
+
+type StatefulGoroutineWriteOp struct {
+	key  int
+	val  int
+	resp chan bool
+}
+
+func StatefulGoroutine() {
+	var readOps uint64
+	var writeOps uint64
+
+	reads := make(chan StatefulGoroutineReadOp)
+	writes := make(chan StatefulGoroutineWriteOp)
+
+	go func() {
+		var state = make(map[int]int)
+		for {
+			select {
+			case read := <-reads:
+				read.resp <- state[read.key]
+			case write := <-writes:
+				state[write.key] = write.val
+				write.resp <- true
+			}
+		}
+	}()
+
+	for r := 0; r < 100; r++ {
+		go func() {
+			for {
+				read := StatefulGoroutineReadOp{
+					key:  rand.Intn(5),
+					resp: make(chan int),
+				}
+				reads <- read
+				<-read.resp
+				atomic.AddUint64(&readOps, 1)
+				time.Sleep(time.Microsecond)
+			}
+		}()
+	}
+	for w := 0; w < 10; w++ {
+		go func() {
+			for {
+				write := StatefulGoroutineWriteOp{
+					key:  rand.Intn(5),
+					val:  rand.Intn(100),
+					resp: make(chan bool),
+				}
+				writes <- write
+				<-write.resp
+				atomic.AddUint64(&writeOps, 1)
+				time.Sleep(time.Microsecond)
+			}
+		}()
+	}
+
+	time.Sleep(time.Second)
+	readOpsFinal := atomic.LoadUint64(&readOps)
+	fmt.Println("readOps:", readOpsFinal)
+	writeOpsFinal := atomic.LoadUint64(&writeOps)
+	fmt.Println("writeOps:", writeOpsFinal)
+}
+
+func Sorting() {
+	strs := []string{"c", "a", "b"}
+	sort.Strings(strs)
+	fmt.Println("Strings:", strs)
+
+	ints := []int{7, 2, 4}
+	sort.Ints(ints)
+	fmt.Println("Ints:", ints)
+
+	s := sort.IntsAreSorted(ints)
+	fmt.Println("sorted:", s)
+}
+
+type byLength []string
+
+func (s byLength) Len() int {
+	return len(s)
+}
+func (s byLength) Swap(i, j int) {
+	temp := s[i]
+	s[i] = s[j]
+	s[j] = temp
+	//s[i], s[j] = s[j], s[i]
+}
+func (s byLength) Less(i, j int) bool {
+	return len(s[i]) < len(s[j])
+}
+
+func SortingByFunction() {
+	fruits := []string{"peach", "banana", "1111111kiwi"}
+	var bylength byLength = fruits
+	//bylength = fruits
+	sort.Sort(bylength) // 传入一个类，调用类里的Less
+
+	fmt.Println(bylength)
+}
+
+func Panic() {
+	//panic("a problem")
+
+	_, err := os.Create("/tmp/file")
+	if err != nil {
+		panic(err)
+	}
+}
+
+func CreateFile(p string) *os.File {
+	fmt.Println("creating")
+	f, err := os.Create(p)
+	if err != nil {
+		panic(err)
+	}
+	return f
+}
+
+func WriteFile(f *os.File) {
+	fmt.Println("writing")
+	p, err := os.ReadDir("e:/")
+	if err != nil {
+		fmt.Fprintf(f, "error:%v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Fprintln(f, "dir list:")
+	for _, v := range p {
+		fmt.Fprintln(f, v.Name())
+	}
+	fmt.Fprintln(f, "dir list done")
+}
+
+func CloseFile(f *os.File) {
+	fmt.Println("closing")
+	err := f.Close()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error:%v\n", err)
+		os.Exit(1)
+	}
+}
+
+func Defer() {
+	f := CreateFile("e:/defer.txt")
+	defer CloseFile(f)
+	WriteFile(f)
+}
+
+func mayPanic() {
+	panic("a problem")
+}
+
+func Recover() {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("recoverd.error:\n", r)
+		}
+	}()
+
+	mayPanic()
+	fmt.Println("After mayPanic()")
+}
+
+func StringFunctions() {
+	var p = fmt.Println
+	p("Contains:  ", strings.Contains("test", "es"))
+	p("Count:     ", strings.Count("test", "t"))
+	p("HasPrefix: ", strings.HasPrefix("test", "te"))
+	p("HasSuffix: ", strings.HasSuffix("test", "st"))
+	p("Index:     ", strings.Index("test", "e"))
+	p("Join:      ", strings.Join([]string{"a", "b"}, "-"))
+	p("Repeat:    ", strings.Repeat("a", 5))
+	p("Replace:   ", strings.Replace("foo", "o", "0", -1))
+	p("Replace:   ", strings.Replace("foo", "o", "0", 1))
+	p("Split:     ", strings.Split("a-b-c-d-e", "-"))
+	p("ToLower:   ", strings.ToLower("TEST"))
+	p("ToUpper:   ", strings.ToUpper("test"))
+}
+
+type point struct {
+	x, y int
+}
+
+func StringFormat() {
+	p := point{1, 2}
+
+	fmt.Printf("struct1: %v\n", p)
+
+	fmt.Printf("struct2: %+v\n", p)
+
+	fmt.Printf("struct3: %#v\n", p)
+
+	fmt.Printf("type: %T\n", p)
+
+	fmt.Printf("bool: %t\n", true)
+
+	fmt.Printf("int: %d\n", 123)
+
+	fmt.Printf("bin: %b\n", 14)
+
+	fmt.Printf("char: %c\n", 33)
+
+	fmt.Printf("hex: %x\n", 456)
+
+	fmt.Printf("float1: %f\n", 78.9)
+
+	fmt.Printf("float2: %e\n", 123400000.0)
+	fmt.Printf("float3: %E\n", 123400000.0)
+
+	fmt.Printf("str1: %s\n", "\"string\"")
+
+	fmt.Printf("str2: %q\n", "\"string\"")
+
+	fmt.Printf("str3: %x\n", "hex this")
+
+	fmt.Printf("pointer: %p\n", &p)
+
+	fmt.Printf("width1: |%6d|%6d|\n", 12, 345)
+
+	fmt.Printf("width2: |%6.2f|%6.2f|\n", 1.2, 3.45)
+
+	fmt.Printf("width3: |%-6.2f|%-6.2f|\n", 1.2, 3.45)
+
+	fmt.Printf("width4: |%6s|%6s|\n", "foo", "b")
+
+	fmt.Printf("width5: |%-6s|%-6s|\n", "foo", "b")
+
+	s := fmt.Sprintf("sprintf: a %s", "string")
+	fmt.Println(s)
+
+	fmt.Fprintf(os.Stderr, "io: an %s\n", "error")
+}
+
+type Jsonresponse1 struct {
+	Page   int
+	Fruits []string
+}
+
+type Jsonresponse2 struct {
+	Page   int      `json:"page"`
+	Fruits []string `json:"fruits"`
+}
+
+func Json() {
+
+	bolB, _ := json.Marshal(true)
+	fmt.Println(string(bolB))
+
+	intB, _ := json.Marshal(1)
+	fmt.Println(string(intB))
+
+	fltB, _ := json.Marshal(2.34)
+	fmt.Println(string(fltB))
+
+	strB, _ := json.Marshal("gopher")
+	fmt.Println(string(strB))
+
+	slcD := []string{"apple", "peach", "pear"}
+	slcB, _ := json.Marshal(slcD)
+	fmt.Println(string(slcB))
+
+	mapD := map[string]int{"apple": 5, "lettuce": 7}
+	mapB, _ := json.Marshal(mapD)
+	fmt.Println(string(mapB))
+
+	res1D := &Jsonresponse1{
+		Page:   1,
+		Fruits: []string{"apple", "peach", "pear"}}
+	res1B, _ := json.Marshal(res1D)
+	fmt.Println(string(res1B))
+
+	res2D := &Jsonresponse2{
+		Page:   1,
+		Fruits: []string{"apple", "peach", "pear"}}
+	res2B, _ := json.Marshal(res2D)
+	fmt.Println(string(res2B))
+
+	byt := []byte(`{"num":6.13,"strs":["a","b"]}`)
+
+	var dat map[string]interface{}
+
+	if err := json.Unmarshal(byt, &dat); err != nil {
+		panic(err)
+	}
+	fmt.Println(dat)
+
+	num := dat["num"].(float64)
+	fmt.Println(num)
+
+	strs := dat["strs"].([]interface{})
+	str1 := strs[0].(string)
+	fmt.Println(str1)
+
+	str := `{"page": 1, "fruits": ["apple", "peach"]}`
+	res := Jsonresponse2{}
+	json.Unmarshal([]byte(str), &res)
+	fmt.Println(res)
+	fmt.Println(res.Fruits[0])
+
+	enc := json.NewEncoder(os.Stdout)
+	d := map[string]int{"apple": 5, "lettuce": 7}
+	enc.Encode(d)
+}
+
+func Time() {
+
+	p := fmt.Println
+
+	now := time.Now()
+	p(now)
+
+	then := time.Date(
+		2009, 11, 17, 20, 34, 58, 651387237, time.UTC)
+	p(then)
+
+	p(then.Year())
+	p(then.Month())
+	p(then.Day())
+	p(then.Hour())
+	p(then.Minute())
+	p(then.Second())
+	p(then.Nanosecond())
+	p(then.Location())
+
+	p(then.Weekday())
+
+	p(then.Before(now))
+	p(then.After(now))
+	p(then.Equal(now))
+
+	diff := now.Sub(then)
+	p(diff)
+
+	p(diff.Hours())
+	p(diff.Minutes())
+	p(diff.Seconds())
+	p(diff.Nanoseconds())
+
+	p(then.Add(diff))
+	p(then.Add(-diff))
+}
+
+func EpochTime() {
+
+	now := time.Now()
+	fmt.Println(now)
+
+	fmt.Println(now.Unix())
+	fmt.Println(now.UnixMilli())
+	fmt.Println(now.UnixNano())
+
+	fmt.Println(time.Unix(now.Unix(), 0))
+	fmt.Println(time.Unix(0, now.UnixNano()))
+}
+
+func TimeFormat() {
+	p := fmt.Println
+
+	t := time.Now()
+	p(t.Format(time.RFC3339))
+
+	t1, e := time.Parse(
+		time.RFC3339,
+		"2012-11-01T22:08:41+00:00")
+	p(t1)
+
+	p(t.Format("3:04PM"))
+	p(t.Format("Mon Jan _2 15:04:05 2006"))
+	p(t.Format("2006-01-02T15:04:05.999999-07:00"))
+	form := "3 04 PM"
+	t2, e := time.Parse(form, "8 41 PM")
+	p(t2)
+
+	fmt.Printf("%d-%02d-%02dT%02d:%02d:%02d-00:00\n",
+		t.Year(), t.Month(), t.Day(),
+		t.Hour(), t.Minute(), t.Second())
+
+	ansic := "Mon Jan _2 15:04:05 2006"
+	_, e = time.Parse(ansic, "8:41PM")
+	p(e)
+}
+
+func RandomNumbers() {
+
+	fmt.Print(rand.Intn(100), ",")
+	fmt.Print(rand.Intn(100))
+	fmt.Println()
+
+	fmt.Println(rand.Float64())
+
+	fmt.Print((rand.Float64()*5)+5, ",")
+	fmt.Print((rand.Float64() * 5) + 5)
+	fmt.Println()
+
+	s1 := rand.NewSource(time.Now().UnixNano())
+	r1 := rand.New(s1)
+
+	fmt.Print(r1.Intn(100), ",")
+	fmt.Print(r1.Intn(100), ",")
+	fmt.Print(r1.Intn(100))
+	fmt.Println()
+
+	s2 := rand.NewSource(42)
+	r2 := rand.New(s2)
+	fmt.Print(r2.Intn(100), ",")
+	fmt.Print(r2.Intn(100))
+	fmt.Println()
+
+	s3 := rand.NewSource(42)
+	r3 := rand.New(s3)
+	fmt.Print(r3.Intn(100), ",")
+	fmt.Print(r3.Intn(100))
+}
+
+func NumberParsing() {
+
+	f, _ := strconv.ParseFloat("1.234", 64)
+	fmt.Println(f)
+
+	i, _ := strconv.ParseInt("123", 0, 64)
+	fmt.Println(i)
+
+	d, _ := strconv.ParseInt("0x1c8", 0, 64)
+	fmt.Println(d)
+
+	u, _ := strconv.ParseUint("789", 0, 64)
+	fmt.Println(u)
+
+	k, _ := strconv.Atoi("135")
+	fmt.Println(k)
+
+	_, e := strconv.Atoi("wat")
+	fmt.Println(e)
+
+}
+
+func UrlParsing() {
+
+	s := "postgres://user:pass@host.com:5432/path?k=v#f"
+
+	u, err := url.Parse(s)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(u.Scheme)
+
+	fmt.Println(u.User)
+	fmt.Println(u.User.Username())
+	p, _ := u.User.Password()
+	fmt.Println(p)
+
+	fmt.Println(u.Host)
+	host, port, _ := net.SplitHostPort(u.Host)
+	fmt.Println(host)
+	fmt.Println(port)
+
+	fmt.Println(u.Path)
+	fmt.Println(u.Fragment)
+
+	fmt.Println(u.RawQuery)
+	m, _ := url.ParseQuery(u.RawQuery)
+	fmt.Println(m)
+	fmt.Println(m["k"][0])
+	fmt.Println(m["k"])
+}
+
+func Sha256Hash() {
+
+	s := "sha256 this string"
+
+	h := sha256.New()
+
+	h.Write([]byte(s))
+
+	bs := h.Sum(nil)
+
+	fmt.Println(s)
+	fmt.Printf("%x\n", bs)
+}
+
+func Base64Encoding() {
+
+	data := "abc123!?$*&()'-=@~"
+
+	sEnc := b64.StdEncoding.EncodeToString([]byte(data))
+	fmt.Println(sEnc)
+
+	sDec, _ := b64.StdEncoding.DecodeString(sEnc)
+	fmt.Println(string(sDec))
+	fmt.Println()
+
+	uEnc := b64.URLEncoding.EncodeToString([]byte(data))
+	fmt.Println(uEnc)
+	uDec, _ := b64.URLEncoding.DecodeString(uEnc)
+	fmt.Println(string(uDec))
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func ReadingFile() {
+
+	dat, err := os.ReadFile("e:/dat")
+	check(err)
+	fmt.Println(string(dat))
+
+	f, err := os.Open("e:/dat")
+	check(err)
+
+	b1 := make([]byte, 5)
+	n1, err := f.Read(b1)
+	check(err)
+	fmt.Printf("%d bytes: %s\n", n1, string(b1[:n1]))
+
+	o2, err := f.Seek(6, 0)
+	check(err)
+	b2 := make([]byte, 10)
+	n2, err := f.Read(b2)
+	check(err)
+	fmt.Printf("%d bytes @ %d: ", n2, o2)
+	fmt.Printf("%v\n", string(b2[:n2]))
+
+	o3, err := f.Seek(6, 0)
+	check(err)
+	b3 := make([]byte, 2)
+	n3, err := io.ReadAtLeast(f, b3, 2)
+	check(err)
+	fmt.Printf("%d bytes @ %d: %s\n", n3, o3, string(b3))
+
+	_, err = f.Seek(0, 0)
+	check(err)
+
+	r4 := bufio.NewReader(f)
+	b4, err := r4.Peek(5)
+	check(err)
+	fmt.Printf("5 bytes: %s\n", string(b4))
+
+	f.Close()
+}
+
+func WriteFile2() {
+
+	d1 := []byte("hello\ngo\n")
+	err := os.WriteFile("e:/dat1", d1, 0644)
+	check(err)
+
+	f, err := os.Create("e:/dat2")
+	check(err)
+
+	defer f.Close()
+
+	d2 := []byte{115, 111, 109, 101, 10}
+	n2, err := f.Write(d2)
+	check(err)
+	fmt.Printf("wrote %d bytes\n", n2)
+
+	n3, err := f.WriteString("writes\n")
+	check(err)
+	fmt.Printf("wrote %d bytes\n", n3)
+
+	f.Sync()
+
+	w := bufio.NewWriter(f)
+	n4, err := w.WriteString("buffered\n")
+	check(err)
+	fmt.Printf("wrote %d bytes\n", n4)
+
+	w.Flush()
+
+}
+
+func FilePath() {
+
+	p := filepath.Join("dir1", "dir2", "filename")
+	fmt.Println("p:", p)
+
+	fmt.Println(filepath.Join("dir1//", "filename"))
+	fmt.Println(filepath.Join("dir1/../dir1", "filename"))
+
+	fmt.Println("Dir(p):", filepath.Dir(p))
+	fmt.Println("Base(p):", filepath.Base(p))
+
+	fmt.Println(filepath.IsAbs("dir/file"))
+	fmt.Println(filepath.IsAbs("/dir/file"))
+
+	filename := "config.json"
+
+	ext := filepath.Ext(filename)
+	fmt.Println(ext)
+
+	fmt.Println(strings.TrimSuffix(filename, ext))
+
+	rel, err := filepath.Rel("a/b", "a/b/t/file")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(rel)
+
+	rel, err = filepath.Rel("a/b", "a/c/t/file")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(rel)
+}
+
+func visit(p string, info os.FileInfo, err error) error {
+	if err != nil {
+		return err
+	}
+	fmt.Println(" ", p, info.Name(), info.IsDir())
+	return nil
+}
+
+func Directories() {
+
+	err := os.Mkdir("subdir", 0755)
+	check(err)
+
+	defer os.RemoveAll("subdir")
+
+	createEmptyFile := func(name string) {
+		d := []byte("")
+		check(os.WriteFile(name, d, 0644))
+	}
+
+	createEmptyFile("subdir/file1")
+
+	err = os.MkdirAll("subdir/parent/child", 0755)
+	check(err)
+
+	createEmptyFile("subdir/parent/file2")
+	createEmptyFile("subdir/parent/file3")
+	createEmptyFile("subdir/parent/child/file4")
+
+	c, err := os.ReadDir("subdir/parent")
+	check(err)
+
+	fmt.Println("Listing subdir/parent")
+	for _, entry := range c {
+		fmt.Println(" ", entry.Name(), entry.IsDir())
+	}
+
+	err = os.Chdir("subdir/parent/child")
+	check(err)
+
+	c, err = os.ReadDir(".")
+	check(err)
+
+	fmt.Println("Listing subdir/parent/child")
+	for _, entry := range c {
+		fmt.Println(" ", entry.Name(), entry.IsDir())
+	}
+
+	err = os.Chdir("../../..")
+	check(err)
+
+	fmt.Println("Visiting subdir")
+	err = filepath.Walk("subdir", visit)
+}
+
+/* func EmbedDirective() {
+
+	//go:embed folder/single_file.txt
+	var fileString string
+
+	//go:embed folder/single_file.txt
+	var fileByte []byte
+
+	//go:embed folder/single_file.txt
+	//go:embed folder/*.hash
+	var folder embed.FS
+
+	print(fileString)
+	print(string(fileByte))
+
+	content1, _ := folder.ReadFile("folder/file1.hash")
+	print(string(content1))
+
+	content2, _ := folder.ReadFile("folder/file2.hash")
+	print(string(content2))
+
+} */
+
 func main() {
-	Tickers()
+	FilePath()
 }

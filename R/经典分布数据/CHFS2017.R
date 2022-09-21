01291849fc999c0b3e7e5447733510b2c7172796
yutaoli <1213617849@qq.com>
2022-09-21 10:19:00 +0800

    人均月收入分布图

--- R/经典分布数据/CHFS2017.R
+++ R/经典分布数据/CHFS2017.R
@@ -2,6 +2,7 @@
 # 中国的收入分布是什么样的？
 # https://zhuanlan.zhihu.com/p/146359406
 
+###函数###
 topN<-function(topN_c, income_rank){
   total_income<-sum(income_rank$total_income_aver_range*income_rank$count);
   total_people<-sum(income_rank$count)
@@ -25,6 +26,45 @@ topN<-function(topN_c, income_rank){
   return(res)
 }
 
+SumN<-function(count_c){
+  res<-NA
+  total<-0
+  for (i in seq_along(count_c)){
+    total<-total+count_c[i]
+    
+    if (i==1) {
+      res<-cbind(res,count_c[i])
+      next# 即continue;
+    }
+    
+    res<-cbind(res,total)
+  }
+  res<-res[c(-1)]#把第一列剔除
+  return(res)
+}
+
+GetDistbute<-function(income_c,sum_total_percentage,segment){#segment区间步长
+  
+  res<-NA
+  res_percentage<-NA
+  for (i in seq_along(income_c)){
+    income_c_i <- income_c[i]
+    if (income_c_i == 0){
+      next
+    }
+    if(income_c_i - floor(income_c_i /segment)*segment == 0){# income_c_i  mod segment == 0
+      res<-cbind(res,income_c_i)
+      res_percentage<-cbind(res_percentage, sum_total_percentage[i])
+    }
+  }
+  res<-res[c(-1)]#把第一列剔除
+  res_percentage<-res_percentage[c(-1)]#把第一列剔除
+  
+  res<-data.frame(res,res_percentage)
+  return (res)
+}
+
+###主程序###
 #x<-c(3,4,6,7,8,9,10,100,3,4,6,7,8,9,10,100)
 #y<-quantile(x,c(0.1,0.2,0.3,.4,.5,.6,.7,.8,.9,.95,.99,.999))
 #y
@@ -73,19 +113,32 @@ hh_income_with_weight_w<-cbind(hh_income_with_weight_w,no_of_people,total_income
 #hh_income_with_weight<-cbind(hh_income_with_weight,total_income_range)#加入一行
 View(hh_income_with_weight_w)
 
-total_income_aver_range_count=sqldf("select total_income_aver_range, count(*) as line_count,sum(swgt_p) as count from hh_income_with_weight_w group by total_income_aver_range")
+total_income_aver_range_count=sqldf("select total_income_aver_range, count(*) as line_count,sum(swgt_p) as count from hh_income_with_weight_w group by total_income_aver_range order by total_income_aver_range asc")
 total_percentage<-total_income_aver_range_count$count/sum(total_income_aver_range_count$count)#占比
-total_income_aver_range_count<-cbind(total_income_aver_range_count,total_percentage)
+sum_total_percentage<-SumN(total_percentage)
+total_income_aver_range_count<-cbind(total_income_aver_range_count,total_percentage, sum_total_percentage)
 View(total_income_aver_range_count)
+
+res<-GetDistbute(total_income_aver_range_count$total_income_aver_range/12,sum_total_percentage,500)#家庭人均月收入累计区间分布
+res
+
+#家庭人均月收入占比累计图
+plot(res$res,
+  res$res_percentage,
+  type="b",
+  main="家庭人均月收入占比累计图",
+  #sub="单位：元",
+  xlab="月收入",
+  ylab="累计占比")
 
 #家庭人均年收入排名
 income_rank=sqldf("select * from total_income_aver_range_count order by total_income_aver_range desc")
 View(income_rank)
 
-barplot(income_rank$total_income_aver_range,
-        main="家庭人均年收入",
-        xlab="收入排名",
-        ylab="家庭人均年收入")
+# barplot(income_rank$total_income_aver_range,
+#         main="家庭人均年收入",
+#         xlab="收入排名",
+#         ylab="家庭人均年收入")
 
 #家庭人均年收入区间分布图
 
@@ -128,6 +181,6 @@ weighted_quantile_p
 #top1%、top10%、top20、top30的人占了多少财富
 topN_c=c(0.01,0.05,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9)
 res<-topN(topN_c,income_rank)
-res<-cbind(topN_c,res)
+res<-data.frame(topN_c,res)
 res
 

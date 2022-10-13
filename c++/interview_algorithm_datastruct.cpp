79f559fc2e77803585ba9f0d9b9de4ceb5a20be6
yutaoli <1213617849@qq.com>
2022-10-13 13:09:18 +0800

    a

---
+++ c++/interview_algorithm_datastruct.cpp
@@ -0,0 +1,35 @@
+/* 
+把思想，翻译成代码，而代码有边界条件，所以要用特殊案例来测试；
+翻译为代码一般的套路：
+	• 循环不变式；=>通过for循环实现；
+	• 初始条件、边界条件；=>通过增加几个变量实现；
+	• 把代码写完，拿特殊案例测试； */
+
+struct Node
+{
+    struct  Node * next;
+    int data;
+    /* data */
+};
+
+Node * ReverseLink(Node *head)
+{
+    if(head == NULL )  return NULL;
+
+    Node * pre = NULL;
+    Node * next = NULL;
+    while(head)
+    {
+        next = head->next;
+        head->next = pre;
+        pre = head;
+        head = next;
+    }
+    return pre;
+}
+
+int main()
+{
+
+    return 0;
+}
